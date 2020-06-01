50e8892cff97b8fdef344479b88afe815ff25e15
ghautham <ghautham.sam01@gmail.com>
2020-06-01 03:51:52 -0400

    added more changes

--- demoWeb.py
+++ demoWeb.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
+    return render_template("index.html")
+
+@app.route('/index.html')
+def index():
     return render_template("index.html")
 
 
