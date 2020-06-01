8917e57a5b630a6684681a8905d2f49d528a0c80
ghautham <ghautham.sam01@gmail.com>
2020-05-31 22:28:50 -0400

    added more content

--- demoWeb.py
+++ demoWeb.py
@@ -8,6 +8,10 @@ def home():
     return render_template("index.html")
 
 
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
